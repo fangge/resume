aebd815c2abae9bbb1bc7fb4156ec909509b25aa
方阳 <fangyang@MacBook-Pro.local>
2022-03-29 01:34:27 +0800

    修改问题

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,26 +1,54 @@
+const fs = require('fs');
 const path = require('path');
+const { spawnSync } = require('child_process');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.base');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const { useTiger } = require('@huyafed/tiger-utils/webpack');
+const EndWebpackPlugin = require('end-webpack-plugin');
+const findChrome = require('chrome-finder');
+const ghpages = require('gh-pages');
 
+function publishGhPages() {
+  return new Promise((resolve, reject) => {
+    ghpages.publish(outputPath, { dotfiles: true }, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    })
+  });
+}
+const outputPath = path.resolve(__dirname, '../dist');
 const prodConfig = {
   mode: process.env.NODE_ENV,
   devtool: 'eval',
   output: {
     filename: 'js/[name].js',
-    // path: path.resolve(__dirname, '../dist'),
-    path: useTiger.outputPath(),
-    publicPath: useTiger.publicPath(),
+    path: path.resolve(__dirname, '../dist'),
     clean: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new FriendlyErrorsWebpackPlugin(),
-    useTiger.createManifest(),
+    new EndWebpackPlugin(async () => {
+      // 自定义域名
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.mrfangge.com');
+
+      await publishGhPages();
+
+      // 调用 Chrome 渲染出 PDF 文件
+      const chromePath = findChrome();
+      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
+        'https://resume.mrfangge.com' // 这里注意改成你的在线简历的网站
+      ]);
+
+      // 重新发布到 ghpages
+      await publishGhPages();
+    }),
   ],
   optimization: {
     minimizer: [
